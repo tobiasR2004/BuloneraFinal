96220f3d88ee6336fd098575cfe6777c910f7dba
tobiasR2004 <tobi.26romani@gmail.com>
2024-10-09 21:51:11 -0300

    "crud 1/2"

--- Bulonera/src/main/java/Bulonera/Persistence/controladoraPersistencia.java
+++ Bulonera/src/main/java/Bulonera/Persistence/controladoraPersistencia.java
@@ -6,6 +6,9 @@ package Bulonera.Persistence;
 
 import Bulonera.Persistence.exceptions.NonexistentEntityException;
 import Bulonera.logica.cabecera_remito;
+import Bulonera.logica.cliente;
+import Bulonera.logica.cuenta_corriente;
+import Bulonera.logica.detalle_remito;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -27,6 +30,8 @@ public class controladoraPersistencia {
     
     public controladoraPersistencia() {
     }
+    
+    //CRUD CABECERA_REMITO
     
      public void crearcabecremito(cabecera_remito idcabec) {
            cabecera_remitoJpa.create(idcabec); 
@@ -56,5 +61,102 @@ public class controladoraPersistencia {
         List<cabecera_remito> listaC = cabecera_remitoJpa.findcabecera_remitoEntities();
         ArrayList<cabecera_remito> listacabecremi = new ArrayList<cabecera_remito>(listaC);
         return listacabecremi;
+    }
+
+    
+    //CRUD CLIENTE
+    
+    public void crearCliente(cliente cliente1) {
+        clienteJpa.create(cliente1);
+    }
+
+    public void eliminarCliente(int id) {
+       try{
+         clienteJpa.destroy(id);
+       } catch(NonexistentEntityException ex) {
+            Logger.getLogger(controladoraPersistencia.class.getName()).log(Level.SEVERE,null, ex);
+       }
+    }
+
+    public void modifCliente(cliente cliente1) {
+        try {
+           clienteJpa.edit(cliente1);      
+        } catch (Exception ex){
+           Logger.getLogger(controladoraPersistencia.class.getName()).log(Level.SEVERE,null, ex);
+        }
+    }
+
+    public cliente consultarcliente(int id) {
+        return clienteJpa.findcliente(id);
+    }
+
+    public ArrayList<cliente> consultarClienList() {
+        List<cliente> listaClient = clienteJpa.findclienteEntities();
+        ArrayList<cliente> listaClientes = new ArrayList<cliente>(listaClient);
+        return listaClientes;
+    }
+    
+    //CRUD
+
+    public void crearCc(cuenta_corriente cC) {
+        cuenta_corrienteJpa.create(cC);
+    }
+
+    public void eliminarCc(int id) {
+        try {
+            cuenta_corrienteJpa.destroy(id);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(controladoraPersistencia.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void modifCc(cuenta_corriente cC1) {
+        try {
+            cuenta_corrienteJpa.edit(cC1);
+        } catch (Exception ex) {
+            Logger.getLogger(controladoraPersistencia.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public cuenta_corriente consultarCc(int id) {
+        return cuenta_corrienteJpa.findcuenta_corriente(id);
+    }
+
+    public ArrayList<cuenta_corriente> consultarCcList() {
+        List<cuenta_corriente> listaCuentacorr = cuenta_corrienteJpa.findcuenta_corrienteEntities();
+        ArrayList<cuenta_corriente> listaCc = new ArrayList<cuenta_corriente>(listaCuentacorr);
+        return listaCc;
+    }
+
+    //CRUD DETALLE REMITO
+    
+    public void crearDetalle(detalle_remito detalle1) {
+        detalle_remitoJpa.create(detalle1);
+    }
+
+    public void eliminarDetalle(int id) {
+        try {
+            detalle_remitoJpa.destroy(id);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(controladoraPersistencia.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void modifDetalle(detalle_remito detalle1) {
+        try {
+            detalle_remitoJpa.edit(detalle1);
+        } catch (Exception ex) {
+            Logger.getLogger(controladoraPersistencia.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public detalle_remito consultarDetalle(int id) {
+      return detalle_remitoJpa.finddetalle_remito(id);
+    }
+
+    public ArrayList<detalle_remito> consultarDetalleList() {
+       List<detalle_remito> listadet = detalle_remitoJpa.finddetalle_remitoEntities();
+       ArrayList<detalle_remito> listadetalle = new ArrayList<detalle_remito>(listadet);
+       return listadetalle;
     }
 }
